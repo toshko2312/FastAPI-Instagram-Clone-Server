a250ae623aba87b72a20ef79288dc9dc84029ce2
Todor Milushev <toshko_2312@abv.bg>
2023-10-16 19:31:56 +0300

    Added password hashing

--- services/user_services.py
+++ services/user_services.py
@@ -1,13 +1,14 @@
 from sqlalchemy.orm import Session
 from db.models import DbUser
 from db.schemas import UserBase
+from db.hashing import Hash
 
 
 def create_user_service(db: Session, request: UserBase):
     new_user = DbUser(
         username=request.username,
         email=request.email,
-        password=request.password
+        password=Hash.bcrypt(request.password)
     )
     db.add(new_user)
     db.commit()
