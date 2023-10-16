f3b58d3b892ecdfce97f2d0f19c6851f8ed91447
Todor Milushev <toshko_2312@abv.bg>
2023-10-16 23:44:23 +0300

    Added posts/upload (uploads an image)

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from fastapi import FastAPI
 from db import models
 from db.database import engine
 from routers import user, post
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(post.router)
+
+app.mount('/images', StaticFiles(directory='images'), name='images')
 
 
 @app.get('/')
--- routers/post.py
+++ routers/post.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, UploadFile, File
 from sqlalchemy.orm import Session
 from services.post_services import create_post_service, get_all_posts_service
 from db.schemas import PostBase, PostDisplay
 from db.database import get_db
 from typing import Annotated, List
+import random
+import string
+import shutil
 
 router = APIRouter(
     prefix='/post',
@@ -19,3 +22,15 @@ def create(request: PostBase, db: Annotated[Session, Depends(get_db)]):
 @router.get('/all', response_model=List[PostDisplay])
 def posts(db: Annotated[Session, Depends(get_db)]):
     return get_all_posts_service(db)
+
+
+@router.post('/image')
+def upload_image(image: Annotated[UploadFile, File(...)]):
+    rand_str = ''.join(random.choice(string.ascii_letters) for _ in range(6))
+    new = f'_{rand_str}.'
+    filename = new.join(image.filename.rsplit('.', 1))
+    path = f'images/{filename}'
+
+    with open(path, 'w+b') as buffer:
+        shutil.copyfileobj(image.file, buffer)
+    return {'filename': path}
